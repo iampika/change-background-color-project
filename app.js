952fccf467bc0ce16c6b36df8373ca8b4ea293a5
Mahendra Choudhary <jakepintu@gmail.com>
2020-07-23 22:23:28 +0530

    add IIFE and constants

---
+++ app.js
@@ -0,0 +1,15 @@
+;(function () {
+  const body = document.querySelector('body')
+  const button = document.querySelector('button')
+  const colors = [
+    '#ffffdd',
+    '#fccbcb',
+    '#ffeadb',
+    '#f7c5a8',
+    '#ff9a76',
+    '#e8505b',
+    '#cbeaed',
+    '#bbe1fa',
+    '#d3dbff',
+  ]
+})()
